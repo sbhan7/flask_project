36542b9336cd5fcb567ac6ee08364d7aa88a46b6
sbhan7 <sbhan.slimi@gmail.com>
2022-09-19 13:07:31 +0430

    add database an orm in flask

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from crypt import methods
+from enum import unique
 import os
 from flask import Flask, render_template, request, make_response
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -7,6 +10,20 @@ app = Flask(__name__)
 path = os.path.join("uploads/photo")
 os.makedirs(path, exist_ok=True)
 
+# config database
+file_dir = os.path.dirname(__file__)
+db_dir = os.path.join(file_dir, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + db_dir
+db = SQLAlchemy(app)
+
+# class ORM like django
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    
+# FOR ONCE YOU CREATE THE DATABASE
+# db.create_all()
     
 #check file format
 allowed_format = {'txt','jpg','png'}
@@ -16,21 +33,23 @@ def check_file_format(fileName):
 # check cookies set or not
 @app.route('/')
 def index():
-   if request.cookies.get('user_email'):
-       return f"hello {request.cookies.get('user_email')}"
-   else:
-       return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users = users)
 
 
 #Get Data from URL and set Cookies
-@app.route('/result', methods=['POST'])
+@app.route('/result', methods=['GET','POST'])
 def show_result():
-    email = request.form['email']
-    password = request.form['password']
-
     try:
-      response = make_response('sucssful ...')
-      response.set_cookie('user_email', email)
+      username = request.form['username']
+      email = request.form['eamil']
+    #   password = request.form['password']
+      user = User(username = username, email = email)
+      db.session.add(user)
+      db.session.commit()
+      
+      response = make_response('sucssful save to db...') #this line can set render_template
+    #   response.set_cookie('user_email', email)
       return response
     except Exception as e:
       return f"ooops error is  {e}"
@@ -58,3 +77,7 @@ def upload_result():
 @app.errorhandler(404)
 def show_error(error):
     return render_template('error_page.html'), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
